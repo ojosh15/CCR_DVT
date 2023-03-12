59a71b31988232379bb6b1070b301bb65a9e153d
ojosh15 <josh.outhous@gmail.com>
2023-03-12 14:07:35 -0400

    Add 3d plot

--- ccr_dashboard/routes.py
+++ ccr_dashboard/routes.py
@@ -7,7 +7,6 @@ import plotly
 import plotly.graph_objects as go
 import plotly.express as px
 from CCR.ccr import ccr
-import pandas as pd
 import glob
 import re
 
@@ -20,41 +19,39 @@ from ccr_dashboard.forms import analysisConfigurationForm, settingsForm
 def analyze():
     analysis_form = analysisConfigurationForm()
     settings_form = settingsForm()
-    unit14 = ccr(14,'LA')
-    target_dir = r'C:\\Users\\joshua.outhous\\Documents\\CCR\\Unit 14\\Main\\'
-    ccr_unit = 'main'
-    p1 = re.compile(r'J\d+_')
-    p2 = re.compile(r'_J\d+')
-    for file_name in glob.glob(target_dir+'*.csv'):
-        m1 = p1.search(file_name)
-        m2 = p2.search(file_name)
-        inputJack = m1.group()[:-1]
-        outputJack = m2.group()[1:]
-        df = pd.read_csv(file_name)
-        data = [df.iloc[:,0],df.iloc[:,1]]
-        unit14.add_data(ccr_unit,inputJack,outputJack,data)
-    freq = unit14.mainUnit['J2_J4']['Oct 2022'][0]
-    mag = unit14.mainUnit['J2_J4']['Oct 2022'][1]
+
+    l = 1001
+    N = np.linspace(0,1000,l,dtype=int)
+    freq = N
+    mag = np.random.rand(l)
+
+    freq1 = N
+    mag1 = np.random.rand(l)
     
-    freq1 = unit14.mainUnit['J2_J5']['Oct 2022'][0]
-    mag1 = unit14.mainUnit['J2_J5']['Oct 2022'][1]
+    item = np.full(shape=l,fill_value=1,dtype=np.int)
+    item1 = np.full(shape=l,fill_value=2,dtype=np.int)
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=freq, y=mag, line = dict(color='yellow', width=2)))
-    fig.add_trace(go.Scatter(x=freq1, y=mag1, line = dict(color='red', width=2)))
+    fig.add_trace(go.Scatter3d(x=item, y=freq, z=mag, mode='lines', line = dict(color='yellow', width=2)))
+    fig.add_trace(go.Scatter3d(x=item1, y=freq1, z=mag1, mode='lines', line = dict(color='red', width=2)))
 
-    fig.update_layout(plot_bgcolor="black",
-                   paper_bgcolor="#363535",
-                   height=800,
-                   title=dict(text="Power Vs. Frequency",x=0.5,xanchor="center",yanchor="top",font=dict(color="white",size=24)),
-                   font_family="Arial",
-                   margin=dict(l=20,r=20,t=50,b=20),
-                   legend=dict(yanchor="top",y=0.99,xanchor="right",x=0.99,bgcolor="rgba(0,0,0,0)",font=dict(color="white",size=14)),
-                   modebar=dict(add=["togglespikelines","hovercompare","v1hovermode"]),
-                   hovermode="closest",
-                   hoverlabel=dict(font=dict(color="white",size=12),bgcolor="#363535"))
-    fig.update_xaxes(title=dict(text="Frequency (MHz)",font=dict(color="white",size=16)),mirror=True,ticks="outside",showline=True,zeroline=False,color="white")
-    fig.update_yaxes(title=dict(text="Power (dBm)",font=dict(color="white",size=16)),mirror=True,ticks="outside",showline=True,zeroline=False,color="white")
+    fig.update_layout(scene=dict(xaxis=dict(title=dict(text="Items",font=dict(color="white",size=16)),mirror=True,ticks="outside",showline=True,zeroline=False,backgroundcolor='black',color='lightgrey'),
+                                 yaxis=dict(title=dict(text="Frequency (MHz)",font=dict(color="white",size=16)),mirror=True,ticks="outside",showline=True,zeroline=False,backgroundcolor='black',color='lightgrey'),
+                                 zaxis=dict(title=dict(text="Power (dBm)",font=dict(color="white",size=16)),mirror=True,ticks="outside",showline=True,zeroline=False,backgroundcolor='black',color='lightgrey'),
+                                 camera_eye_z=0.75),
+                    plot_bgcolor="black",
+                    paper_bgcolor="#363535",
+                    height=650,
+                    title=dict(text="Power Vs. Frequency",x=0.5,xanchor="center",yanchor="top",font=dict(color="white",size=24)),
+                    font_family="Arial",
+                    margin=dict(l=0, r=0, b=0, t=0),
+                    legend=dict(yanchor="top",y=0.9,xanchor="right",x=0.99,bgcolor="rgba(0,0,0,0)",font=dict(color="white",size=14)),
+                    modebar=dict(add=["togglespikelines","hovercompare","v1hovermode"]),
+                    hovermode="closest",
+                    hoverlabel=dict(font=dict(color="white",size=12),bgcolor="#363535"))
+    #fig.update_xaxes(title=dict(text="Items",font=dict(color="white",size=16)),mirror=True,ticks="outside",showline=True,zeroline=False,color="white")
+    #fig.update_yaxes(title=dict(text="Frequency (MHz)",font=dict(color="white",size=16)),mirror=True,ticks="outside",showline=True,zeroline=False,color="white")
+    #fig.update_zaxes(title=dict(text="Power (dBm)",font=dict(color="white",size=16)),mirror=True,ticks="outside",showline=True,zeroline=False,color="white")
 
     graphJson = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
